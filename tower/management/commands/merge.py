a536b385f9c0717bf479c1a04ed4624d1389a652
Staś Małolepszy <stas@mozilla.com>
2010-06-16 20:34:11 +0200

    Add the merge command (which is more generic than amalgamate in that it doesn't handle Zamboni's r-* and z-* files) and the verbatimize command which copies the POT files created by extract to templates/LC_MESSAGES

---
+++ tower/management/commands/merge.py
@@ -0,0 +1,82 @@
+import os
+from subprocess import Popen
+from tempfile import TemporaryFile
+
+from django.core.management.base import BaseCommand
+
+from manage import settings
+
+try:
+    domains = [settings.TEXT_DOMAIN] + settings.STANDALONE_DOMAINS
+except AttributeError:
+    domains = [settings.TEXT_DOMAIN, 'javascript']
+
+class Command(BaseCommand):
+    """
+    This will update all locales' PO files by merging them with the POT files.
+    """
+
+    def handle(self, *args, **options):
+
+        locale_dir = os.path.join(settings.ROOT, 'locale')
+
+        for domain in domains:
+
+            print "Merging %s strings to each locale..." % domain
+            domain_pot = os.path.join(locale_dir, 'templates', 'LC_MESSAGES',
+                                      '%s.pot' % domain)
+            if not os.path.isfile(domain_pot):
+                sys.exit("Can't find %s.pot" % domain)
+
+            for locale in os.listdir(locale_dir):
+                if (not os.path.isdir(os.path.join(locale_dir, locale)) or
+                    locale.startswith('.') or 
+                    locale == 'templates' or
+                    locale == 'compendia'):
+                            continue
+
+                compendium = os.path.join(locale_dir, 'compendia', 
+                                          '%s.compendium' % locale)
+                domain_po = os.path.join(locale_dir, locale, 'LC_MESSAGES',
+                                         '%s.po' % domain)
+
+                if not os.path.isfile(domain_po):
+                    print " Can't find (%s).  Creating..." % (domain_po)
+                    p1 = Popen(["msginit",
+                                "--no-translator",
+                                "--locale=%s" % locale,
+                                "--input=%s" % domain_pot,
+                                "--output-file=%s" % domain_po,
+                                "--width=200",])
+                    p1.communicate()
+
+                print "Merging %s.po for %s" % (domain, locale)
+
+                domain_pot_file = open(domain_pot)
+
+                if locale == "en_US":
+                    enmerged = TemporaryFile('w+t')
+                    p2 = Popen(["msgen", "-"], stdin=domain_pot_file,
+                            stdout=enmerged)
+                    p2.communicate()
+                    mergeme = enmerged
+                else:
+                    mergeme = domain_pot_file
+
+                mergeme.seek(0)
+                command = ["msgmerge",
+                           "--update",
+                           #"--no-fuzzy-matching",
+                           "--sort-output",
+                           "--width=200",
+                           domain_po,
+                           "-"]
+                if os.path.isfile(compendium):
+                    print "(using a compendium)"
+                    command.insert(1, '--compendium=%s' % compendium)
+                p3 = Popen(command, stdin=mergeme)
+                p3.communicate()
+                mergeme.close()
+            print "Domain %s finished" % domain
+
+        print "All finished"
