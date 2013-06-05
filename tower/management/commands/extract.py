cf1034cacc2b9248740748e5cd375f26b64f5d1a
Will Kahn-Greene <willkg@mozilla.com>
2013-06-05 16:59:52 -0400

    Move extract_tower_python and extract_tower_template
    Moving these to the base makes it way easier to configure tower.
    For example, instead of this:
    
        DOMAIN_METHODS = {
            'messages': [
                ('fjord/**.py', 'tower.management.commands.extract.extract_tower_python'),
            ]
        }
    
    we can do this:
    
        DOMAIN_METHODS = {
            'messages': [
                ('fjord/**.py', 'tower.extract_tower_python'),
            ]
        }
    
    I added an import to tower.management.commands.extract so that older
    tower configurations continue to work.

--- tower/management/commands/extract.py
+++ tower/management/commands/extract.py
@@ -1,17 +1,19 @@
 import os
+import tempfile
 from optparse import make_option
 from subprocess import Popen
-import tempfile
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
-from jinja2 import ext
-from babel.messages.extract import (DEFAULT_KEYWORDS, extract_from_dir,
-                                    extract_python)
+from babel.messages.extract import (DEFAULT_KEYWORDS, extract_from_dir)
 from translate.storage import po
 
-from tower import strip_whitespace, add_context, split_context
+from tower import split_context
+
+# These are here for backwards compatibility with older iterations of
+# tower.
+from tower import extract_tower_python, extract_tower_template
 
 DEFAULT_DOMAIN = 'all'
 
@@ -42,56 +44,6 @@ OPTIONS_MAP = {
 }
 
 COMMENT_TAGS = ['L10n:']
-
-
-def tweak_message(message):
-    """We piggyback on jinja2's babel_extract() (really, Babel's extract_*
-    functions) but they don't support some things we need so this function will
-    tweak the message.  Specifically:
-
-        1) We strip whitespace from the msgid.  Jinja2 will only strip
-            whitespace from the ends of a string so linebreaks show up in
-            your .po files still.
-
-        2) Babel doesn't support context (msgctxt).  We hack that in ourselves
-            here.
-    """
-    if isinstance(message, basestring):
-        message = strip_whitespace(message)
-    elif isinstance(message, tuple):
-        # A tuple of 2 has context, 3 is plural, 4 is plural with context
-        if len(message) == 2:
-            message = add_context(message[1], message[0])
-        elif len(message) == 3:
-            if all(isinstance(x, basestring) for x in message[:2]):
-                singular, plural, num = message
-                message = (strip_whitespace(singular),
-                           strip_whitespace(plural),
-                           num)
-        elif len(message) == 4:
-            singular, plural, num, ctxt = message
-            message = (add_context(ctxt, strip_whitespace(singular)),
-                       add_context(ctxt, strip_whitespace(plural)),
-                       num)
-    return message
-
-
-def extract_tower_python(fileobj, keywords, comment_tags, options):
-    for lineno, funcname, message, comments in \
-            list(extract_python(fileobj, keywords, comment_tags, options)):
-
-        message = tweak_message(message)
-
-        yield lineno, funcname, message, comments
-
-
-def extract_tower_template(fileobj, keywords, comment_tags, options):
-    for lineno, funcname, message, comments in \
-            list(ext.babel_extract(fileobj, keywords, comment_tags, options)):
-
-        message = tweak_message(message)
-
-        yield lineno, funcname, message, comments
 
 
 def create_pounit(filename, lineno, message, comments):
