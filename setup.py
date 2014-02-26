ade9804345eb49dd1b626fd276cd9572fd15d1b3
Dave Dash <dave@pinterest.com>
2014-02-26 09:30:56 -0800

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tower',
-    version='0.4.0',
+    version='0.4.1',
     description='Pull strings from a variety of sources, collapse whitespace, '
                 'support context (msgctxt), and merging .pot files.',
     long_description=open('README.rst').read(),
